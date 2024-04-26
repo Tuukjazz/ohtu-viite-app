2b94c48d296c4a3470ad3bebd87018d0d32d2861
neuvonej <eemeli.neuvonen@gmail.com>
2024-04-26 17:11:46 +0300

    kaikki formin kentät saadaan pythonin puolelle
    Co-Authored-By: vtluukkw <valtteri.luukkala@gmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-viitelista = []
+viitelista = [] # Tähän voidaan myöhemmin tallentaa viite-olioita.
 
 @app.route("/")
 def home():
@@ -9,10 +9,17 @@ def home():
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    testi = request.form["testi"]
-    viitelista.append(testi)
+    # Tallennetaan formin kenttien sisällöt muuttujiin. Muuttujien arvoilla voidaan sitten luoda viite-olio.
+    author = request.form["author"]
+    title = request.form["title"]
+    journal = request.form["journal"]
+    year = request.form["year"]
+    volume = request.form["volume"]
+    pages = request.form["pages"]
+    # Tässä demotaan, että arvot on tosiaan saatu...
+    viitelista.append(f"({author}, {title}, {journal}, {year}, {volume}, {pages})")
     return home()
 
-# Tämä vaaditaan jos ohjelman ajaa: "poetry run python app.py". (Toinen vaihtoehto: "python -m flask run")
+# Tämä vaaditaan jos ohjelman ajaa: "poetry run python app.py" (Toinen vaihtoehto: "python -m flask run")
 if __name__ == "__main__":
     app.run(debug=True)
