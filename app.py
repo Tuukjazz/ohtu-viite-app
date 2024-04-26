fb1c35459545195f01f3f420effaa7d3ccd50fac
neuvonej <eemeli.neuvonen@gmail.com>
2024-04-26 14:14:58 +0300

    html form --> python demottu

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+viitelista = []
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", vl=viitelista)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    testi = request.form["testi"]
+    viitelista.append(testi)
+    return home()
+
+# Tämä vaaditaan jos ohjelman ajaa: "poetry run python app.py". (Toinen vaihtoehto: "python -m flask run")
+if __name__ == "__main__":
+    app.run(debug=True)
