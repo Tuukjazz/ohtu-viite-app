f8dec1a9cb475954564034bcd171da9ba5f854de
Tuukjazz <tuukjazz@student.jyu.fi>
2024-05-23 22:41:45 +0300

    changed how bibtex prints

--- formatteri.py
+++ formatteri.py
@@ -69,6 +69,8 @@ def muuttaja(lista, tyyppi=None):
                 bibtex_entry += f"  title = {{{viite[2]}}},\n"
                 bibtex_entry += f"  journal = {{{viite[4]}}},\n"
                 bibtex_entry += f"  year = {{{viite[3]}}},\n"
+                bibtex_entry += f"  volume = {{{viite[5]}}},\n"
+                bibtex_entry += f"  pages = {{{viite[6]}}},\n"
                 bibtex_entry += f"}}"
                 ml.append(bibtex_entry)
 
